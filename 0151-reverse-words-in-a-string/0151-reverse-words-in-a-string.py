4de3bb7e9d83343e41eb08459adb1a22f4df383d
adityarana14 <49364001+adityarana14@users.noreply.github.com>
2023-12-26 17:00:52 -0500

    Time: 36 ms (79.07%), Space: 17.5 MB (5.24%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,25 +1,30 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        list_words = []
-        answer = ""
-        i = 0 
-        while i <= len(s) - 1:
-            temp_word = ""
-            if s[i] != ' ':
-                while  i <= len(s) - 1 and s[i] != ' ':
-                    temp_word = temp_word + s[i]
-                    i = i + 1 
-            else: 
-                i = i + 1 
-            if temp_word != "":
-                list_words.append(temp_word)
+        # list_words = []
+        # answer = ""
+        # i = 0 
+        # while i <= len(s) - 1:
+        #     temp_word = ""
+        #     if s[i] != ' ':
+        #         while  i <= len(s) - 1 and s[i] != ' ':
+        #             temp_word = temp_word + s[i]
+        #             i = i + 1 
+        #     else: 
+        #         i = i + 1 
+        #     if temp_word != "":
+        #         list_words.append(temp_word)
             
                     
-        # print(list_words)
-        for i in range(len(list_words)-1 , -1 , -1):
-            answer = answer + list_words[i]
-            if i != 0:
-                answer = answer + " "
+        # # print(list_words)
+        # for i in range(len(list_words)-1 , -1 , -1):
+        #     answer = answer + list_words[i]
+        #     if i != 0:
+        #         answer = answer + " "
 
+        check = s.split()
+        print(check)
+        check = check[::-1]
+        print(check)
+        answer = " ".join(check)
         return answer
         
