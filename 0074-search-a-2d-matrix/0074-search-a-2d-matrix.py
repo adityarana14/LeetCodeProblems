2b0c383ec102b5dbfd4f9d606537b27130703637
adityarana14 <49364001+adityarana14@users.noreply.github.com>
2024-06-16 10:56:44 -0400

    Time: 44 ms (67.71%), Space: 16.9 MB (92.74%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,27 +1,44 @@
 class Solution:
+
+    def binary_search(self, list_matrix, target):
+        left = 0 
+        right = len(list_matrix) - 1 
+
+        while left <= right: 
+            mid = (left + right) // 2 
+            if list_matrix[mid] > target: 
+                right = mid - 1 
+            elif list_matrix[mid] < target: 
+                left = mid + 1 
+            else: 
+                return True 
+        return False 
+
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if target < matrix[0][0] or target > matrix[len(matrix)-1][len(matrix[0])-1]:
+            return False 
 
-        col = len(matrix[0]) - 1
-        for i in range(len(matrix)):
+        rows = len(matrix) - 1 
+        cols = len(matrix[0]) - 1
 
-            if(target <= matrix[i][col]):
-                start = 0
-                end = col
-                
-                while(start <= end):
-                    mid = (start + end ) // 2
-                    if(target == matrix[i][mid]):
+        top = 0 
+        bottom = len(matrix) - 1
+
+        while top <= bottom: 
+            mid = (top + bottom) // 2
+
+            if target > matrix[mid][0]:
+                if target <= matrix[mid][cols]:
+                    bool_answer = self.binary_search(matrix[mid], target)
+                    if bool_answer == True: 
                         return True
+                    else: 
+                        return False 
+                else: 
+                    top = mid + 1 
+            elif target < matrix[mid][0]:
+                bottom = mid - 1 
+            else: 
+                return True 
 
-                    if(target > matrix[i][mid]):
-                        start = mid + 1 
-                    else: end = mid - 1
-
-
-        return False            
-
-
-                        
-            
-
-        
+        return False  
