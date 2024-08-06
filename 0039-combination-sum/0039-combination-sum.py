45195e0e92012a63704613496ce87e31f3996eea
adityarana14 <49364001+adityarana14@users.noreply.github.com>
2024-08-06 18:31:53 -0400

    Time: 66 ms (16.82%), Space: 16.6 MB (67.38%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def __init__(self):
+        self.answer = []
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.combGenerator(candidates, [], 0, target)
+        return self.answer
+        
+    def combGenerator(self, candidates, output, sum_num, target):
+        if sum_num == target: 
+            self.answer.append(output)
+            return 
+        if sum_num > target: 
+            return 
+        if not candidates: 
+            return 
+        
+        output_copy = output.copy()
+        output_copy.append(candidates[0]) if candidates else None 
+
+        self.combGenerator(candidates[:], output_copy, sum_num + candidates[0], target) 
+        self.combGenerator(candidates[1:], output.copy(), sum_num, target) 
+
+        return 
